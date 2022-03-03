e714f989372b9cfe86a16858751d7882bf9cd78e
Jamie <jxxthomson@gmail.com>
2022-03-03 18:13:52 +0000

    EW 1 + 2

--- MusicByLocation/LocationHandler.swift
+++ MusicByLocation/LocationHandler.swift
@@ -30,7 +30,7 @@ class LocationHandler: NSObject, CLLocationManagerDelegate {
         if let firstLocation = locations.first {
             geocoder.reverseGeocodeLocation(firstLocation, completionHandler: { (placemarks, error) in
                 if error != nil {
-                    self.stateController?.lastKnownLocation = "Could not perform lookup of location from coordinate information"
+                    self.stateController?.lastKnownLocation = "Could not perform looku p of location from coordinate information"
                 } else {
                     if let firstPlacemark = placemarks?[0] {
                         self.stateController?.lastKnownLocation = firstPlacemark.locality ?? "no local place"
